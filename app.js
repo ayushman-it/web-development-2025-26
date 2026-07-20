897a02a004a6eaef78b697714cef78eb6f825bcf
Ayushman <avenuedesign768@gmail.com>
2026-07-20 19:58:47 +0530

    added

--- app.js
+++ app.js
@@ -1,1 +1,100 @@
-console.log("hello")
+// var (Keyword) 
+var name; // Declearation
+var name = "Pratham"; 
+var name = "Priyanshu"
+name = "Chetan"; // Assignment
+
+// console.log(name)
+// let (Keyword)
+
+let city;
+
+city = "Bilaspur";
+city = "Bhopal";
+city = "Chhindwara";
+city = "Seoni";
+
+// let city = "Raipur";
+
+// console.log(city)
+
+// const (Keyword)
+const student_id = 4565;
+
+// const student_id = 89393;
+
+// DATA TYPES 
+// 1. Premetive 
+// 1. number, 
+let age = 67;
+
+// 2. string, 
+let fname = "Ayushman";
+
+
+// 3. boolean
+let deleteData = false;
+
+
+// 4. undefined
+let unknown;
+
+// 5. null
+
+let statement = null
+// console.log(typeof(statement))
+
+// 2. Non Premetive
+// a. array
+// -------------0---------1------------2----------3-------------
+// let group = ["Chetan", "Pratham", "Priyanshu", "Aadarsh"]
+// console.log(group[3])
+// b. object
+
+let emp = [
+    {
+        name:"chetan",
+        age: 21,
+        salary: 2344,
+        city: "waraseoni"
+    },
+    {
+        name:"Pratham",
+        age: 21,
+        salary: 2344,
+        city: "waraseoni"
+    },
+    {
+        name:"Aadarsh",
+        age: 21,
+        salary: 2344,
+        city: "waraseoni"
+    },
+    {
+        name:"Lokesh",
+        age: 21,
+        salary: 2344,
+        city: "waraseoni"
+    }
+]
+
+
+console.log(emp[1].name)
+console.log(emp[1].age)
+console.log(emp[1].salary)
+console.log(emp[1].city)
+
+
+// alert("Hello TCM")
+
+// console.log("TCM Students")
+
+// document.write("Helllo")
+
+// let name= prompt("Enter Your Name")
+
+// let status = confirm("Are you want to delete this data ?")
+// ___________________________________________________
+
+// DOM - Document Object Modal
+
